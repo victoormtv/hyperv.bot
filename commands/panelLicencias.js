71dde4babd2e70369506af9a4bdfd461bee380cb
victoormtv <victorsanchezmontalvan@gmail.com>
2026-09-15 23:11:53 -0500

    feat: add panel-gratis command and utility to send free panel message

--- commands/panelLicencias.js
+++ commands/panelLicencias.js
@@ -1,12 +1,12 @@
 const { SlashCommandBuilder } = require("discord.js");
-const { enviarPanelLicenciasWindows } = require("../utils/panelLicencias.js");
+const { enviarPanelGratis } = require("../utils/panelGratis.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("panel-licencias")
-        .setDescription("Envía el panel de licencias de Windows"),
+        .setName("panel-gratis")
+        .setDescription("Envía el panel gratis"),
     async execute(interaction) {
         await interaction.reply({ content: "Enviando panel...", ephemeral: true });
-        await enviarPanelLicenciasWindows(interaction.channel);
+        await enviarPanelGratis(interaction.channel);
     },
 };
--- utils/panelGratis.js
+++ utils/panelGratis.js
@@ -11,11 +11,31 @@ const {
 } = require("discord.js");
 const config = require("../data/config.js");
 
-async function enviarPanelLicenciasWindows(channel) {
+async function enviarPanelGratis(channel) {
     const container = new ContainerBuilder()
         .setAccentColor(config.embedColor)
+        .addMediaGalleryComponents(
+            new MediaGalleryBuilder().addItems(
+                new MediaGalleryItemBuilder().setURL(
+                    "https://www.realcloudx.com/Cloud/tanatozn/panel-free.png",
+                ),
+            ),
+        )
         .addTextDisplayComponents(
-            new TextDisplayBuilder().setContent("> HyperV - Panel Gratis"),
+            new TextDisplayBuilder().setContent("### HyperV - Panel Gratis"),
+        )
+        .addTextDisplayComponents(
+            new TextDisplayBuilder().setContent(
+                "Obtén acceso directo a tu panel gratis y actívalo siguiendo los pasos.",
+            ),
+        )
+        .addSeparatorComponents(new SeparatorBuilder())
+        .addTextDisplayComponents(
+            new TextDisplayBuilder().setContent(
+                "> Descarga los recursos necesarios.\n" +
+                "> Estar dentro del canal de voz para poder generar tu licencia de Panel Graris.\n" +
+                "> Conéctate en <#1548459490574082098>."
+            ),
         )
         .addSeparatorComponents(new SeparatorBuilder())
         .addTextDisplayComponents(
@@ -33,17 +53,12 @@ async function enviarPanelLicenciasWindows(channel) {
                         .setStyle(ButtonStyle.Link)
                         .setURL("https://hyperv.online/gratis"),
                 ),
-        )
-        .addSeparatorComponents(new SeparatorBuilder())
-        .addMediaGalleryComponents(
-            new MediaGalleryBuilder().addItems(
-                new MediaGalleryItemBuilder().setURL(config.defaultImage),
-            ),
-        )
+        );
+
     await channel.send({
         components: [container],
         flags: MessageFlags.IsComponentsV2,
     });
 }
 
-module.exports = { enviarPanelLicenciasWindows };
+module.exports = { enviarPanelGratis };
